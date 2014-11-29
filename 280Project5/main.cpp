064ce9957695ce6ca318ad9865d873e9bac6b70b
dylanbp <dylanphillips@Macintosh-2.local>
2014-11-29 16:24:48 -0500

    Initial Commit

---
+++ 280Project5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  280Project5
+//
+//  Created by dylanbp on 11/29/14.
+//  Copyright (c) 2014 Dylan Phillips. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
